d5e3bbb0086e4851cf0fb7634b09555859f340f4
Lu <jetqin_2008@hotmail.com>
2022-03-11 15:41:51 +1100

    bug fix

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react'
-import { Route, useParams, Link, useRouteMatch } from 'react-router-dom'
-import Comments from '../comments/Comments'
-import HighlightedQuote from '../quotes/HighlightedQuote'
-import useHttp from '../../hooks/use-http'
-import { getSingleQuote } from '../../lib/api'
-import LoadingSpinner from '../UI/LoadingSpinner'
+import React, { useEffect } from "react";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
+import Comments from "../comments/Comments";
+import HighlightedQuote from "../quotes/HighlightedQuote";
+import useHttp from "../../hooks/use-http";
+import { getSingleQuote } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 // const DUMMY_QUOTES = [
 //     { id: 'q1', author: 'Max', text: 'Learning react is fun'},
@@ -15,48 +15,58 @@ import LoadingSpinner from '../UI/LoadingSpinner'
 // ]
 
 const QuoteDetail = () => {
-    const params = useParams()
-    const match = useRouteMatch()
+    const params = useParams();
+    const match = useRouteMatch();
 
-    const { quoteId } = params
+    const { quoteId } = params;
 
-    const { sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true)
+    const {
+        sendRequest,
+        status,
+        data: loadedQuote,
+        error,
+    } = useHttp(getSingleQuote, true);
 
     // const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId)
 
     useEffect(() => {
-        sendRequest(quoteId)
-    },[sendRequest, quoteId])
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
 
-    if(status === 'pending') {
+    if (status === "pending") {
         return (
-            <div className='centered'>
+            <div className="centered">
                 <LoadingSpinner />
             </div>
-        )
+        );
     }
 
     if (error) {
-        return <p className='centered'>{error}</p>
+        return <p className="centered">{error}</p>;
     }
 
-    if (loadedQuote.text) {
-        return <p>No quote found</p>
+    if (!loadedQuote.text) {
+        return <p>No quote found</p>;
     }
 
     return (
         <div>
-            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+            <HighlightedQuote
+                text={loadedQuote.text}
+                author={loadedQuote.author}
+            />
             <Route path={`${match.path}`} exact>
-                <div className='centered'>
-                    <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
+                <div className="centered">
+                    <Link className="btn--flat" to={`${match.url}/comments`}>
+                        Load Comments
+                    </Link>
                 </div>
             </Route>
             <Route path={`${match.path}/comments`}>
                 <Comments />
             </Route>
         </div>
-    )
-}
+    );
+};
 
-export default QuoteDetail
+export default QuoteDetail;
